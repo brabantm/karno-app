9e86db0a187de65452559b531763a0a2cd473273
brabantm <37080996+brabantm@users.noreply.github.com>
2023-12-14 13:55:22 +0000

    first commit

--- Hello.py
+++ Hello.py
@@ -13,38 +13,107 @@
 # limitations under the License.
 
 import streamlit as st
+from shapely.geometry import Point, Polygon
+import geopandas as gpd
+import pandas as pd
+import geopy
+import googlemaps
+from math import radians, sin, cos, sqrt, atan2
+
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+def haversine_distance(lat1, lon1, lat2, lon2):
+    # Radius of the Earth in meters
+    R = 6371000.0
+    
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    
+    # Calculate differences in coordinates
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    
+    # Haversine formula
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    
+    # Calculate the distance
+    distance = R * c
+    
+    return distance
 
 def run():
-    st.set_page_config(
+  st.set_page_config(
         page_title="Hello",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
 
-    st.sidebar.success("Select a demo above.")
+  # street = st.sidebar.text_input("Street", "75 Bay Street")
+  # city = st.sidebar.text_input("City", "Toronto")
+  # province = st.sidebar.text_input("Province", "Ontario")
+  # country = st.sidebar.text_input("Country", "Canada")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+  # geolocator = Nominatim(user_agent="GTA Lookup")
+  # # geocode = RateLimiter(geolocator.geocode, min_delay_seconds=5)
+  # location = geolocator.geocode(street+", "+city+", "+province+", "+country)
+
+  # lat = location.latitude
+  # lon = location.longitude
+
+  # map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
+
+  # st.map(map_data)
+
+  api_key = "AIzaSyAOi5CosdhIJItpyKFjD4jzXmV_MWNj-HA"
+  gmaps = googlemaps.Client(key=api_key)
+
+  def autocomplete_address(query):
+      result = gmaps.places_autocomplete(query, components={'country': "BE"})
+      return result
+
+  # df = pd.DataFrame([{"lat": 50.847671, "lon": 4.412759, "radius": 600,"color": (200,150,50,0.3)},  {"lat": 50.727671, "lon": 4.412759, "radius": 50, "color": (255,50,50,1.0)}])
+  # st.dataframe(df)
+  # st.map(df, size="radius", color="color")
+  st.markdown(
+      """
+      ## Les prochains réseaux de chaleurs Karno proche de chez moi
+
+      **👇 Introduisez votre adresse ci-dessous**
     """
-    )
+  )
+
+  address_query = st.text_input("Entrez votre adresse", "")
+
+
+  # Display autocomplete suggestions
+  if address_query:
+      suggestions = autocomplete_address(address_query)
+          
+      # Display clickable list of places
+      selected_place = st.selectbox("Nous avons trouvé les adresses suivantes:", suggestions, format_func=lambda place: place['description'])
+
+      # Show selected place details
+      if selected_place:
+          
+          # Extract latitude and longitude from the selected place
+          location = gmaps.place(selected_place['place_id'])['result']['geometry']['location']
+          lat, lon = location['lat'], location['lng']
+          
+          distance = haversine_distance(lat, lon, 50.847732, 4.414955)
+          st.markdown("distance "+ str(int(distance))+"m")
+          # Display the map centered around the selected place
+          map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
+
+        # Display the map centered around the selected place
+          st.map(map_data, zoom=13, size=30, color=(200,30,30,1.0))
+
+
+
 
 
 if __name__ == "__main__":
